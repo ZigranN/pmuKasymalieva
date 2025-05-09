e828bdb7228a3fd575416e3dd5737b95252b363d
Nargiz <mam.zigran@gmail.com>
2025-05-09 11:07:57 +0600

    Deploy on vercel 1

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ ОБЯЗАТЕЛЬНО './', чтобы пути были относительными
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: true,
+    sourcemap: true
   }
 });
